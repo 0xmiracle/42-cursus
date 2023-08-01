4c768ae999506be8aeffa8a989bdfd0350af35fc
0xmiracle <ramiatwani@icloud.com>
2023-08-01 15:19:07 +0400

    Cleaned up the file

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ratwani <ratwani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 17:37:07 by Ratwani           #+#    #+#             */
-/*   Updated: 2023/07/16 11:31:43 by ratwani          ###   ########.fr       */
+/*   Updated: 2023/07/17 16:23:23 by ratwani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,3 +30,4 @@ void	*ft_memcpy(void *dest, const void *src, size_t len)
 	}
 	return (dest);
 }
+
