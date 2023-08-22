5a4d6db0d75a9ae58b0ef1a418af3efa7558f7b9
0xmiracle <ramiatwani@icloud.com>
2023-08-22 12:54:57 +0400

    Fixed some leaks

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ratwani <ratwani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 15:35:00 by ratwani           #+#    #+#             */
-/*   Updated: 2023/08/17 19:31:17 by ratwani          ###   ########.fr       */
+/*   Updated: 2023/08/22 12:54:04 by ratwani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,16 @@ char	*ft_fltr(char *str)
 
 	j = 0;
 	i = 0;
-	while (str[i] != '\n')
+	while (str[i] != '\n' && str[i] != '\0')
 		i++;
-	tmp = (char *)malloc(sizeof(char) * (i + 1));
+	tmp = (char *)malloc(sizeof(char) * (i + 2));
 	while (str[j] != '\n' && str[j] != '\0')
 	{
 		tmp[j] = str[j];
 		j++;
 	}
+	if (str[i] == '\n')
+		tmp[j++] = '\n';
 	tmp[j] = '\0';
 	return (tmp);
 }
@@ -65,14 +67,14 @@ char	*ft_rmv(char *str)
 	}
 	tmp[j] = '\0';
 	free(str);
-	str = tmp;
-	return (str);
+	return (tmp);
 }
 
 char	*get_next_line(int fd)
 {
 	int			i;
-	static char	*str = NULL;
+	static char	*str;
+	// char		buff[BUFFER_SIZE + 1];
 	char		*buff;
 	int			j;
 	char		*tmp;
@@ -81,9 +83,7 @@ char	*get_next_line(int fd)
 	j = 0;
 	if (!str)
 		str = ft_strdup("");
-	buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buff)
-		return (NULL);
+	buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	while (i > 0 && !ft_strchr(str, '\n'))
 	{
 		i = read(fd, buff, BUFFER_SIZE);
@@ -92,27 +92,45 @@ char	*get_next_line(int fd)
 	}
 	if (i == 0)
 	{
+		if (*str == '\0')
+		{
+			free(str);
+			str = NULL;
+			return (free(buff), NULL);
+		}
+		tmp = ft_fltr(str);
+		str = ft_rmv(str);
+		return (tmp);
+	}
+	if (i == -1)
+	{
 		free(str);
-		free(buff);
-		return (NULL);
+		str = NULL;
+		return (free(buff), NULL);
 	}
 	tmp = ft_fltr(str);
 	str = ft_rmv(str);
+	free(buff); 
 	return (tmp);
 }
 
-// int	main(void)
-// {
-// 	int fd;
-// 	char *c;
-// 	fd = open("test.txt", O_RDONLY);
-// 	if (fd == -1)
-// 	{
-// 		perror("Error opening the file");
-// 		return (1);
-// 	}
-// 	while ((c = get_next_line(fd)) != NULL)
-// 	{
-// 		printf("last: %s\n", c);
-// 	}
-// }
+int	main(void)
+{
+	int fd;
+	char *c;
+	fd = open("test.txt", O_RDONLY);
+	if (fd == 0)
+	{
+		perror("Error opening the file");
+		return (1);
+	}
+	c = get_next_line(fd);
+	while (c != NULL)
+	{
+		printf("last: %s", c);
+		free(c);
+		c = get_next_line(fd);
+	}
+	free(c);
+	close(fd);
+}
